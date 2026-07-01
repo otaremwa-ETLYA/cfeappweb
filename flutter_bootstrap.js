db74a764a6b339442b4c8f871c28dac2f96379a1
otaremwa-ETLYA <otaremwa@pureflowboda.com>
2026-07-01 10:04:38 +0300

    better checkbox, scrolling, logout

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"a10d8ac38de835021c8d2f920dbf50a920ccc0
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "573472116" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "540121126" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
